3594d858c579606ccde6b4559813ecd2a8edf71a
NattePappelo <axel.appelo@gmail.com>
2025-10-07 12:43:28 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+from lib import *
+
+HelloWorld("print")
+
---
+++ lib.py
@@ -0,0 +1,12 @@
+def HelloWorld(x):
+    if x == "print":
+        print("HelloWorld")
+    elif x == "input":
+        return input("HelloWorld")
+    else:
+        print("Error: Edu dum i hela huve?")
+    
+
+
+
+#HelloWorld("print")
